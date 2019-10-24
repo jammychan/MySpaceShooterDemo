8e4f093ea9f4c93123accfa21dbbb728e4d35d55
jammychan <jammy@jammydeMacBook-Pro.local>
2019-10-24 16:40:09 +0800

    随机旋转小行星

---
+++ MySpaceShooterDemo/Assets/Script/RandomRotator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour
+{
+    public float tumble;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.angularVelocity = new Vector3(
+            rb.angularVelocity.x + Random.insideUnitSphere.x * tumble,
+            rb.angularVelocity.y,
+            rb.angularVelocity.z + Random.insideUnitSphere.z * tumble);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
