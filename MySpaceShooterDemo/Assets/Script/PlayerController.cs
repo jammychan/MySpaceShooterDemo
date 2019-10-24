2db6fb6d2fbce78d83070ff38affb29a0c30d39f
jammychan <jammy@jammydeMacBook-Pro.local>
2019-10-24 16:12:45 +0800

    发射子弹

--- MySpaceShooterDemo/Assets/Script/PlayerController.cs
+++ MySpaceShooterDemo/Assets/Script/PlayerController.cs
@@ -15,6 +15,12 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public float tilt;
 
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+
+    private float nextFire;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,7 +30,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        }
     }
 
     void FixedUpdate()
